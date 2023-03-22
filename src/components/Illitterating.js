6b5539e7e27645deae61cc9d6babce1bc31ecac3
Remigijus <you@example.com>
2023-03-22 08:58:46 +0200

    replace timing

--- src/components/Illitterating.js
+++ src/components/Illitterating.js
@@ -8,6 +8,7 @@ import { FaGithub, FaPhone, FaLinkedin } from "react-icons/fa"
 import { BsEnvelopeAt } from "react-icons/bs";
 import Boring from './Boring';
 
+
 const Illiterating = () => {
 
   const [cvCopy, setCvCopy] = useState({});
@@ -19,27 +20,86 @@ const Illiterating = () => {
   const [educationData, setEducationData] = useState([]);
   const [experienceData, setExperienceData] = useState([]);
   const [languagesData, setLanguagesData] = useState([]);
+  const [timeLeft, setTimeLeft] = useState(100)
+  const [timer, setTimer] = useState(false)
+  const [replaced, setReplaced] = useState(false)
+  const [display, setDisplay] = useState([])
 
   useEffect(() => {
+    console.log('use effect evoced')
     setCvCopy(curriculumVitae)
-    console.log('original', curriculumVitae)
-    console.log('copy', cvCopy)
+    // console.log('original', curriculumVitae)
+    // console.log('copy', cvCopy)
     setPersonalData(Object.values(Object.values(curriculumVitae)[0]))
     setSkillsData(Object.values(curriculumVitae)[1])
     // setEducationData(Object.values(Object.values(curriculumVitae)[2][0]).concat(Object.values(Object.values(curriculumVitae)[2][1])))
     setEducationData(Object.values(Object.values(curriculumVitae)[2]).reduce((sum, curr) => sum.concat(Object.values(curr)), []))
 
     setExperienceData(Object.values(Object.values(curriculumVitae)[3]).reduce((sum, curr) => sum.concat(curr), []))
+    console.log(Object.values(Object.values(curriculumVitae)[3]).reduce((sum, curr) => sum.concat(curr), []))
+
 
     setLanguagesData(Object.values(Object.values(curriculumVitae)[4]))
 
-    const timer = setTimeout(() => {
-      setIlliteral(true);
-      clearTimeout(timer);
-    }, 1000)
+    // const timer = setTimeout(() => {
+    //   setIlliteral(true);
+    //   kickLetters(letters)
+    //   clearTimeout(timer);
+    // }, 1000)
   }, [format])
 
+  useEffect(() => {
+    if (timer) {
+      setTimeout(() => {
+        setTimeLeft(timeLeft - 1)
+        // console.log(timeLeft)
+        if (timeLeft === 0) {
+          setTimer(false)
+        }
+        clearTimeout();
+      }, 50)
+    }
+  }, [timer, timeLeft])
 
+  const letters = [['a'], ['b'], ['e']];
+
+
+  const countTime = (arr) => {
+    setTimer(true)
+    setTimeLeft(100)
+    setReplaced(false)
+  }
+  const kickLetters = async (arr) => {
+    setDisplay(letters[0])
+    for (let i = 0; i < arr.length; i++) {
+      await handleTiming(arr[i])
+      setDisplay(arr[i + 1] || ['Nice!'])
+      console.log('change letter in screen', arr[i + 1] || 'over')
+    }
+
+  };
+
+
+  const handleTiming = async (letters) => {
+    countTime()
+    function timeout(delay) {
+      return new Promise(res => setTimeout(res, delay));
+
+    }
+
+    await timeout(6200)
+    letters.forEach(e => {
+      handleReplace(e)
+    });
+    console.log('kick now and wait for 2 seconds', letters)
+
+    setReplaced(true)
+    function timeout1(delay) {
+      return new Promise(res => setTimeout(res, delay));
+    }
+    await timeout1(2000)
+
+  }
 
 
   const replaceLetter = (string, letter) => {
@@ -49,43 +109,48 @@ const Illiterating = () => {
   }
 
 
-  const handleReplace = (letter) => {
-    const nextPersonalData = personalData.map((x) => replaceLetter(x, letter))
+  const handleReplace = (ltr) => {
+    const nextPersonalData = personalData.map((x) => replaceLetter(x, ltr))
     setPersonalData(nextPersonalData)
-    const nextSkillsData = skillsData.map((x) => replaceLetter(x, letter))
+    const nextSkillsData = skillsData.map((x) => replaceLetter(x, ltr))
     setSkillsData(nextSkillsData)
-    const nextEducationData = educationData.map((x) => replaceLetter(x, letter))
+    const nextEducationData = educationData.map((x) => replaceLetter(x, ltr))
     setEducationData(nextEducationData)
-
     // let nextExperienceData = experienceData
     // nextExperienceData.map((record) => {
     //   Object.keys(record).forEach((key) => {
-    //     record[key] = replaceLetter(record[key], letter)
+    //     record[key] = replaceLetter(record[key], ltr)
     //   })
     // });
     // setExperienceData(nextExperienceData)
     // mutates curriculumVitae object  should be replaced 
-
-    const nextLanguagesData = languagesData.map((x) => replaceLetter(x, letter))
+    const nextLanguagesData = languagesData.map((x) => replaceLetter(x, ltr))
     setLanguagesData(nextLanguagesData)
   }
 
 
+  const print = () => {
+    setTimeLeft(99)
+    setReplaced(true)
 
-
-
-
-
+  }
 
   return (
     <div>
-      {<div div className='grid' >
+      <h3 className={`replacing ${replaced ? 'replaced' : ''}`}>{display.map((x, i) => <span key={i} > {x + ''}</span>)}</h3>
+
+      <div className='track'>
+
+        <div className='marker' style={{ maxWidth: `${timeLeft}%` }}></div>
+      </div>
+      {<div className='grid' >
 
         <div className='name'>
+          <button onClick={() => { kickLetters(letters) }}>kick</button>
           <button onClick={() => { handleReplace('a') }}>replace a in  personal data</button>
-          <button onClick={() => { handleReplace('b') }}>replace b in  personal data</button>
           <button onClick={() => { handleReplace('e') }}>replace e in  personal data</button>
           <button onClick={() => { handleReplace('s') }}>replace s in  personal data</button>
+          <button onClick={() => { print() }}>print</button>
 
           <h2>{(personalData[0]) + ' ' + (personalData[1])}</h2>
         </div>
